019f7f86f421305855de30f9e8e162f0298d2d5e
andreabassi78 <andreabassi@polimi.it>
2022-05-14 15:27:34 +0200

    testing 2 functions

--- src/napari_mytest/_tests/test_widget.py
+++ src/napari_mytest/_tests/test_widget.py
@@ -1,33 +1,16 @@
-from napari_mytest import ExampleQWidget, example_magic_widget
+from napari_mytest import function0, function1
 import numpy as np
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
     
-def test_example_magic_widget(make_napari_viewer, capsys):
+def test_widgets(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    
+    
+    test_widget0 = function0()
+    test_widget1 = function1()
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    test_widget1(3)
+    # captured = capsys.readouterr()
+    
+    assert test_widget0.c.value == 6
--- src/napari_mytest/_widget.py
+++ src/napari_mytest/_widget.py
@@ -1,41 +1,68 @@
 """
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/plugins/stable/guides.html#widgets
-
-Replace code below according to your needs.
+Test code for napari plugin
 """
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+
 from magicgui import magic_factory
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
+@magic_factory
+def function0(a: int = 2, c: int = 0):
+    pass
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def function1(b: int = 3):
+    a = widget0.a.value 
+    c = a*b 
+    widget0.c.value = c 
+    
+
+# this will not be executed by the plugin: widget0.a will not exists there
+if __name__ == '__main__':
+    import napari
+    viewer = napari.Viewer()
+
+    widget0 = function0()
+    widget1 = function1()
+    
+    viewer.window.add_dock_widget(widget0, name = 'mywidget 0',
+                                  area='right', add_vertical_stretch=True)
+    viewer.window.add_dock_widget(widget1, name = 'mywidget 1',
+                                  area='right', add_vertical_stretch=True)
+    
+    napari.run()
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+
+
+
+
+
+# class ExampleQWidget(QWidget):
+#     # your QWidget.__init__ can optionally request the napari viewer instance
+#     # in one of two ways:
+#     # 1. use a parameter called `napari_viewer`, as done here
+#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+#     def __init__(self, napari_viewer):
+#         super().__init__()
+#         self.viewer = napari_viewer
+
+#         btn = QPushButton("Click me!")
+#         btn.clicked.connect(self._on_click)
+
+#         self.setLayout(QHBoxLayout())
+#         self.layout().addWidget(btn)
+
+#     def _on_click(self):
+#         print("napari has", len(self.viewer.layers), "layers")
+
+
+# @magic_factory
+# def example_magic_widget(img_layer: "napari.layers.Image"):
+#     print(f"you have selected {img_layer}")
+
+
+# # Uses the `autogenerate: true` flag in the plugin manifest
+# # to indicate it should be wrapped as a magicgui to autogenerate
+# # a widget.
+# def example_function_widget(img_layer: "napari.layers.Image"):
+#     print(f"you have selected {img_layer}")
