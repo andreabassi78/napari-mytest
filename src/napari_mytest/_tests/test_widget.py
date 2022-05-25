cb6871170c74f71e153e746fa7eb2ea4bed70d56
andreabassi78 <andreabassi@polimi.it>
2022-05-25 08:25:59 +0200

    testing shared variable

--- src/napari_mytest/_tests/test_widget.py
+++ src/napari_mytest/_tests/test_widget.py
@@ -1,18 +1,11 @@
 from napari_mytest import function0, function1
-import numpy as np
     
 def test_widgets(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    data = np.random.random ([3,100,100])
-    im_layer = viewer.add_image(data)
     test_widget0 = function0()
     test_widget1 = function1()
 
     test_widget0(3)
-    test_widget1(im_layer)
-    
-    
-    
-    # captured = capsys.readouterr()
-    
-    assert im_layer.scale[0] == 3 
+    test_widget1(4)
+     
+    out, err = capsys.readouterr()
+    assert out == 12
--- src/napari_mytest/_widget.py
+++ src/napari_mytest/_widget.py
@@ -3,34 +3,25 @@ Test code for napari plugin
 """
 
 from magicgui import magic_factory
-from napari.layers import Image
-
 
 @magic_factory
 def function0(a: int = 5):
     my_shared_vars.c = a
-    print(my_shared_vars.c)
+    print('c=', my_shared_vars.c)
 
 
 @magic_factory
-def function1(image: Image):
-    image.scale = [my_shared_vars.c,1,1]
+def function1(b: int = 3):
+    result = my_shared_vars.c * b
+    print('c*b=', result)
     
-
-
-class InventoryItem:
-    """Class for keeping track of an item in inventory."""
-    name: str
-    unit_price: float
-    quantity_on_hand: int = 0
-
 
 from dataclasses import dataclass
 @dataclass
 class Shared_variables:
-    c: int = 15
+    c: int 
 
-my_shared_vars = Shared_variables(15)  
+my_shared_vars = Shared_variables(2)  # create instance with just some initial value for c
     
     
 
@@ -43,44 +34,10 @@ if __name__ == '__main__':
     widget1 = function1()
     
     viewer.window.add_dock_widget(widget0, name = 'mywidget 0',
-                                  area='right', add_vertical_stretch=True)
+                                  area='right')
     viewer.window.add_dock_widget(widget1, name = 'mywidget 1',
-                                  area='right', add_vertical_stretch=True)
+                                  area='right')
     
     napari.run()
 
 
-
-
-
-
-
-# class ExampleQWidget(QWidget):
-#     # your QWidget.__init__ can optionally request the napari viewer instance
-#     # in one of two ways:
-#     # 1. use a parameter called `napari_viewer`, as done here
-#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-#     def __init__(self, napari_viewer):
-#         super().__init__()
-#         self.viewer = napari_viewer
-
-#         btn = QPushButton("Click me!")
-#         btn.clicked.connect(self._on_click)
-
-#         self.setLayout(QHBoxLayout())
-#         self.layout().addWidget(btn)
-
-#     def _on_click(self):
-#         print("napari has", len(self.viewer.layers), "layers")
-
-
-# @magic_factory
-# def example_magic_widget(img_layer: "napari.layers.Image"):
-#     print(f"you have selected {img_layer}")
-
-
-# # Uses the `autogenerate: true` flag in the plugin manifest
-# # to indicate it should be wrapped as a magicgui to autogenerate
-# # a widget.
-# def example_function_widget(img_layer: "napari.layers.Image"):
-#     print(f"you have selected {img_layer}")
